d289c974498399d9febbf2bcfdf7b7c57b8f64e2
Joshua Schlichting <42978321+jrs322@users.noreply.github.com>
2019-10-03 17:34:09 -0400

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -17,4 +17,28 @@ def store_checkout(inventory_tuple_list, item_purchase_list):
     else:
         print("Invalid list len")
         return 0
+    
+def highest_frequency_count(item_list):
+    count_dict = {}
+    max_count = 0
+    if (len(item_list) != 0):
+        for item in item_list:
+            found_item = count_dict.get(item, -1)
+            print(found_item)
+            if found_item == -1:
+                count_dict.update({item: 1})
+                if (1 > max_count):
+                    max_count = 1
+            else:
+                prev_count = found_item
+                prev_count = prev_count + 1
+                if prev_count > max_count:
+                    max_count = prev_count
+                #print(str(item) + " , " + str(found_item))
+                count_dict.update({str(item):prev_count})
+        #print(count_dict)
+        return max_count
+    else:
+        print("Empty List")
+        return 0
         
